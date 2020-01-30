12035fffb690492be63b641c16b6b65c13bbe1fd
srna99 <serena.ch1999@gmail.com>
2020-01-30 00:09:52 -0500

    Pick out components of input.

--- eliza.py
+++ eliza.py
@@ -11,28 +11,34 @@ import random
 
 
 # pick from default responses for inputs that are not coded for
-def generate_default_response():
-    print(random.choice(default_resp))
+def generate_default_response() -> str:
+    return random.choice(default_resp)
 
 
 # keywords and their responses
-keywords = {r'\b(need)\b': ["Do you really need {0}?", "What would you do if you got {0}?"],
-            r'\b(hate)\b': ["What part do you hate about {0}?", "What made you hate {0}?"],
-            r'\b(love)\b': ["What do you love the most about {0}?", "When did you start loving {0}?"],
-            r'\b(crave)\b': ["Tell me more about your cravings.", "How did you develop your craving?"],
-            r'\b(want)\b': ["Why do you want {0}?", "What would you do if you got {0]?"],
-            r'\b(family)\b': ["Tell me more about your family.",
-                              "What was your relationship with your family like?"],
-            r'\b(school)\b': ["How are you doing at school?",
-                              "What difficulties do you face in school?"]}
+keywords = {r'\b(need)\b(.*)': ["Do you really need {}?", "What would you do if you got {}?"],
+            r'\b(hate)\b(.*)': ["What part do you hate about {}?", "What made you hate {}?"],
+            r'\b(love)\b(.*)': ["What do you love the most about {}?",
+                                "When did you start loving {}?"],
+            r'\b(crave)\b(.*)': ["Tell me more about your cravings.",
+                                 "How did you develop your craving?"],
+            r'\b(want)\b(.*)': ["Why do you want {}?", "What would you do if you got {}?"],
+            r'\b(family)\b(.*)': ["Tell me more about your family.",
+                                  "What was your relationship with your family like?"],
+            r'\b(school)\b(.*)': ["How are you doing at school?",
+                                  "What difficulties do you face in school?"]}
 # possible patterns in input and their responses
-re_patterns = {r'\b(I)\b(.*)[^,.!?]': "Why do you {0}?", r'\b(I am)\s(.*)[^,.!?]': "Why are you {0}?",
-               r'(I do not know)(.*)?[^,.!?]': "Why don't you know {0}?"}
+re_patterns = {r'\b(i am)\s(.*)': "Why are you {}?",
+               r'(i do not know)(.*)?': "Why don't you know {}?",
+               r'(i will)\s(.*)': "How will you {}?",
+               r'(i feel)\s(.*)': "When do you usually feel {}?",
+               r'\b(i)\b(.*)': "Why do you {}?"}
 # default responses
 default_resp = ["I see.", "Tell me more.", "How does that make you feel?", "Why do you say that?",
                 "Can you elaborate on that?", "I didn't quite understand. Can you explain further?"]
 # edited pronouns
-pronouns = {"i": "you", "am": "are", "my": "your", "was": "were", "we": "you all", "me": "you"}
+pronouns = {"i": "you", "am": "are", "my": "your", "was": "were", "we": "you all", "me": "you",
+            "you": "me", "mine": "yours"}
 
 # greetings and get name
 print("E: Hi, I am the psychotherapist. What is your name?")
@@ -45,22 +51,31 @@ while True:
 
     # user's responses
     response = input("YOU: ")
-    edited_resp = re.sub(r'\s+', ' ', response)
+    edited_resp = re.sub(r'\s+', ' ', response).strip(".!")
     print(edited_resp)
 
-    match = None
+    tokens = None
     eliza_resp = None
-    for pattern in re_patterns:
-        match = re.match(pattern, edited_resp)
 
-        if match:
-            eliza_resp = re_patterns[pattern]
+    for keyword in keywords:
+        tokens = re.search(keyword, edited_resp)
+        if tokens:
+            eliza_resp = random.choice(keywords[keyword])
             break
+    else:
+        for pattern in re_patterns:
+            tokens = re.search(pattern, edited_resp)
+            if tokens:
+                eliza_resp = re_patterns[pattern]
+                break
 
-    if eliza_resp:
-        for i in range(len(match.groups())):
-            # token =
-            pass
+    if eliza_resp is None:
+        eliza_resp = generate_default_response()
+    else:
+        words = tokens.group(len(tokens.groups())).lower().split()
+        for word in words:
+            if word in pronouns:
+                word = pronouns[word]
 
     # "bye" ends conversation
     if re.search(r'\b(bye)\b', response.lower()):
