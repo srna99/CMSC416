c8b5b27b94f339597efbdbb31573f2e94d341817
srna99 <serena.ch1999@gmail.com>
2020-01-29 22:21:51 -0500

    Create list of patterns and pronouns to look out for.

--- eliza.py
+++ eliza.py
@@ -9,18 +9,38 @@ TYPE INTRO HEREEEE!!!
 import re
 import random
 
-# re_patterns = [r'^(.*?)\s(crave)\s(.*?)$':""]
-default_resp = ["I see.", "Tell me more.", "How does that make you feel?", "Why do you say that?", "Can you elaborate on that?"]
 
+# pick from default responses for inputs that are not coded for
+def generate_default_response():
+    print(random.choice(default_resp))
+
+
+# possible patterns in input and their responses
+re_patterns = {r'\b(I)\b(.*)[^,.!?]': "Why do you {0}?", r'\b(I am)\s(.*)[^,.!?]': "Why are you {0}?",
+               r'(I do not know)(.*)?[^,.!?]': "Why don't you know {0}?"}
+
+# default responses
+default_resp = ["I see.", "Tell me more.", "How does that make you feel?", "Why do you say that?",
+                "Can you elaborate on that?", "I didn't quite understand. Can you explain further?"]
+
+# edited pronouns
+pronouns = {"i": "you", "am": "are", "my": "your", "was": "were", "we": "you all", "me": "you"}
+
+# greetings and get name
 print("E: Hi, I am the psychotherapist. What is your name?")
 name = input("YOU: ")
 
 print(f"E: Hi, {name}. How can I help you today?")
 
+# conversation loop
 while True:
-    response = input("YOU: ")
-    print(response)
 
+    # user's responses
+    response = input("YOU: ")
+    edited_resp = re.sub(r'\s+', ' ', response)
+    print(edited_resp)
+
+    # "bye" ends conversation
     if re.match(r'\b(bye)\b', response.lower()):
         print(f"Bye, {name}!")
         break
