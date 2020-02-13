2e1934df8d0c1d190062557bd141b09a5c57bc7e
srna99 <serena.ch1999@gmail.com>
2020-02-13 00:33:39 -0500

    Tag and add starting words to history dict.

--- ngram.py
+++ ngram.py
@@ -13,10 +13,13 @@ Algorithm:
 
 """
 
-import os
 import re
 import random
 import sys
+
+history = {}
+probabilities = {}
+ngram = []
 
 num_for_gram = 0
 num_of_sentences = 0
@@ -24,25 +27,47 @@ text_files = []
 
 for i in range(1, len(sys.argv)):
     if i == 1:
-        num_for_gram = sys.argv[i]
+        num_for_gram = int(sys.argv[i])
     elif i == 2:
-        num_of_sentences = sys.argv[i]
+        num_of_sentences = int(sys.argv[i])
     else:
         text_files.append(sys.argv[i])
 
 print(num_for_gram, num_of_sentences, text_files)
 
 for txt_file in text_files:
-    with open(txt_file, "r", encoding="utf8") as file:
+    with open(txt_file, "r", encoding="utf-8-sig") as file:
         content = file.read().lower()
 
     content = re.sub(r'[,*\-_;:()"]', ' ', content)
-    content = re.sub(r'[.!?]', '.', content)
+    content = re.sub(r'[.!?]', ' .', content)
     content = re.sub(r'\s+', ' ', content)
 
-    print(content)
+    tokens = content.split()
 
+    for i in range(0, 2000):
+        print(i)
+        if i == 0 or tokens[i-1] == ".":
+            for j in range(1, num_for_gram):
+                ngram.append("<s>")
 
+            word = tokens[i]
+            key = " ".join(map(str, ngram))
+
+            if key in history.keys():
+                if word in history[key]:
+                    history[key][word] += 1
+                    print("here")
+                else:
+                    history[key][word] = 1
+                    print("no here")
+            else:
+                history[key] = {word: 1}
+                print("no no here")
+
+            print(history)
+
+            ngram.clear()
 
 
 
