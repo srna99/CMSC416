73f0f0446e625f5fc511458ba34faa561901a73f
srna99 <serena.ch1999@gmail.com>
2020-02-13 22:00:32 -0500

    Create probabilities dictionary.

--- ngram.py
+++ ngram.py
@@ -13,12 +13,13 @@ Algorithm:
 
 """
 
-import re
 import random
+import re
 import sys
 
-history = {}
+ngram_dict = {}
 probabilities = {}
+tokens = []
 ngram = []
 
 num_for_gram = 0
@@ -43,41 +44,53 @@ for txt_file in text_files:
     for _ in range(1, num_for_gram):
         start_tags += "<s> "
 
-    content = re.sub(r'[,*\-_;:()"]', ' ', content)
+    content = re.sub(r'[,*\-_;:()\"]', ' ', content)
 
     content = start_tags + content
     content = re.sub(r'[.!?]', ' .', content)
     edit = content.split(".")
-    end_start_tags = ". " + start_tags
+    end_start_tags = "<e> " + start_tags
     content = end_start_tags.join(map(str, edit))
 
     content = re.sub(r'\s+', ' ', content)
 
-    tokens = content.split()
+    tokens.extend(content.split())
+    # print(len(tokens))
 
-    index = 0
-    while index < 100:
-        ngram.append(tokens[index])
+index = 0
+while index < len(tokens):
+    ngram.append(tokens[index])
 
-        if len(ngram) == num_for_gram:
-            word = ngram.pop()
-            key = " ".join(map(str, ngram))
+    if len(ngram) == num_for_gram:
+        word = ngram.pop()
+        key = " ".join(map(str, ngram))
 
-            if key in history.keys():
-                if word in history[key]:
-                    history[key][word] += 1
-                else:
-                    history[key][word] = 1
+        if key in ngram_dict.keys():
+            if word in ngram_dict[key]:
+                ngram_dict[key][word] += 1
             else:
-                history[key] = {word: 1}
-
-            index -= (num_for_gram - 2)
-            ngram.clear()
+                ngram_dict[key][word] = 1
         else:
-            index += 1
+            ngram_dict[key] = {word: 1}
 
+        index -= (num_for_gram - 2)
+        ngram.clear()
+    else:
+        index += 1
 
+for history, word_dict in ngram_dict.items():
+    n_minus_1_freq = sum(word_dict.values())
 
+    for word, n_freq in word_dict.items():
+        probability = round(n_freq / n_minus_1_freq, 10)
+
+        if history in probabilities.keys():
+            probabilities[history][word] = probability
+        else:
+            probabilities[history] = {word: probability}
+
+# print(ngram_dict)
+# print(probabilities)
 
 
 
